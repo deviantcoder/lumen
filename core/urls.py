2dce4d43657738056b1f51018819e1301138f943
deviant <andrewsafonovpost@gmail.com>
2025-09-09 13:40:02 +0300

    Added password reset

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,4 +11,26 @@ urlpatterns = [
     path('accounts/social/', include('social_django.urls', namespace='social')),
 
     path('', TemplateView.as_view(template_name='feed.html'), name='home'),
+
+    # password reset
+    path(
+        'reset_password/',
+        auth_views.PasswordResetView.as_view(template_name='accounts/password_reset/reset_password.html'),
+        name='reset_password'
+    ),
+    path(
+        'reset_password_sent/',
+        auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset/password_reset_done.html'),
+        name='password_reset_done'
+    ),
+    path(
+        'reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset/password_reset_confirm.html'),
+        name='password_reset_confirm'
+    ),
+    path(
+        'reset_password_complete/',
+        auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset/password_reset_complete.html'),
+        name='password_reset_complete'
+    ),
 ]
