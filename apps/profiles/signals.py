e7e2983cf0d333f24daa8d28fcb8e7b983a8c7b6
deviant <andrewsafonovpost@gmail.com>
2025-09-11 11:21:34 +0300

    Added profile media deletion signal

--- apps/profiles/signals.py
+++ apps/profiles/signals.py
@@ -1,7 +1,9 @@
+import os
+import shutil
 import logging
 
 from django.dispatch import receiver
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import post_save, pre_save, post_delete
 from django.contrib.auth import get_user_model
 
 from .models import Profile
@@ -39,3 +41,14 @@ def compress_profile_image(sender, instance, **kwargs):
         instance.image = compress_image(instance.image)
     except Exception as e:
         logger.warning(f'Image compression failed: {e}')
+
+
+@receiver(post_delete, sender=Profile)
+def delete_profile_media(sender, instance, *args, **kwargs):
+    try:
+        path = f'media/profiles/{str(instance.user.public_id)}'
+
+        if os.path.exists(path):
+            shutil.rmtree(path)
+    except Exception as e:
+        logger.warning(f'User media deletion failed: {e}')
