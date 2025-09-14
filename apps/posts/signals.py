ecf335b5771e5c13a69d9ad3d340cf04cc19745a
deviant <andrewsafonovpost@gmail.com>
2025-09-14 18:39:39 +0300

    Added post media compression and deletion signals

---
+++ apps/posts/signals.py
@@ -0,0 +1,35 @@
+import os
+import shutil
+import logging
+
+from django.dispatch import receiver
+from django.db.models.signals import pre_save, post_delete
+
+from .models import PostMedia, Post
+
+from utils.files import ALLOWED_IMAGE_EXTENSIONS, compress_image
+
+
+logger = logging.getLogger(__name__)
+
+
+@receiver(pre_save, sender=PostMedia)
+def compress_media_file(sender, instance, **kwargs):
+    try:
+        if instance.file:
+            ext = os.path.splitext(instance.file.name)[-1].lower().lstrip('.')
+            if ext in ALLOWED_IMAGE_EXTENSIONS:
+                instance.file = compress_image(instance.file)
+    except Exception as e:
+        logger.warning(f'Image compression failed: {e}')
+
+
+@receiver(post_delete, sender=Post)
+def delete_post_media(sender, instance, *args, **kwargs):
+    try:
+        path = f'media/posts/{str(instance.public_id)}'
+
+        if os.path.exists(path):
+            shutil.rmtree(path)
+    except Exception as e:
+        logger.warning(f'Post media deletion failed: {e}')
