65ef7ad879eac5a47dba31cc87f7de869f92f385
Andrew <andrewsafonovpost@gmail.com>
2025-09-19 16:11:00 +0300

    Added edit profile template

--- apps/profiles/urls.py
+++ apps/profiles/urls.py
@@ -7,6 +7,7 @@ app_name = 'profiles'
 
 
 urlpatterns = [
+    path('edit/', views.edit_profile, name='edit_profile'),
     path('<str:username>/', views.profile, name='profile'),
     path('<str:username>/posts/', views.get_user_posts, name='get_user_posts'),
 ]
--- apps/profiles/views.py
+++ apps/profiles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 
 from django.contrib.auth import get_user_model
@@ -29,8 +29,8 @@ def get_user_posts(request, username):
     user = get_object_or_404(User, username=username)
 
     posts = user.posts.filter(status=Post.POST_STATUSES.ACTIVE)
-
-    if 'saved' in request.GET:
+    
+    if 'saved' in request.GET and user == request.user:
         saved_posts_pks = user.saved_posts.values_list('post', flat=True)
         posts = Post.objects.filter(
             pk__in=saved_posts_pks,
@@ -42,3 +42,14 @@ def get_user_posts(request, username):
     }
 
     return render(request, 'profiles/partials/user_posts_list.html', context)
+
+
+@login_required
+def edit_profile(request):
+    profile = request.user.profile
+
+    context = {
+        'profile': profile,
+    }
+
+    return render(request, 'profiles/edit_profile.html', context)
