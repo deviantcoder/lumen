9aa42e776bc04a36e0d2f7babff8567910f8ec80
Andrew <andrewsafonovpost@gmail.com>
2025-09-19 18:58:37 +0300

    Optimized feed view queries

--- apps/feed/views.py
+++ apps/feed/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.db.models import Exists, OuterRef
+from django.db.models import Exists, OuterRef, Prefetch
 
-from apps.posts.models import Post, Like, Save
+from apps.posts.models import Post, Like, Save, PostMedia, Tag
 
 
 @login_required
 def feed(request):
-    posts = Post.objects.all().annotate(
-        liked=Exists(
-            Like.objects.filter(user=request.user, post=OuterRef('pk'))
-        ),
-        saved=Exists(
-            Save.objects.filter(user=request.user, post=OuterRef('pk'))
+    posts = (
+        Post.objects.all().
+        annotate(
+            liked=Exists(Like.objects.filter(user=request.user, post=OuterRef('pk'))),
+            saved=Exists(Save.objects.filter(user=request.user, post=OuterRef('pk')))
+        )
+        .select_related('author', 'author__profile')
+        .prefetch_related(
+            Prefetch('media'),
+            Prefetch('tags'),
+            'likes'
         )
     )
 
