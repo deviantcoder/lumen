6a944d30138b618151de14d674c7914dd8eabbf1
Andrew <andrewsafonovpost@gmail.com>
2025-10-18 14:36:56 +0300

    Collection cover image compression signal

--- apps/stories/signals.py
+++ apps/stories/signals.py
@@ -5,7 +5,7 @@ import logging
 from django.dispatch import receiver
 from django.db.models.signals import pre_save, post_delete
 
-from .models import Story
+from .models import Story, Collection
 
 from utils.files import (
     ALLOWED_IMAGE_EXTENSIONS, compress_image
@@ -16,7 +16,7 @@ logger = logging.getLogger(__name__)
 
 
 @receiver(pre_save, sender=Story)
-def compress_media_file(sender, instance, **kwargs):
+def compress_story_media_file(sender, instance, **kwargs):
     try:
         if instance.media:
             ext = os.path.splitext(instance.media.name)[-1].lower().lstrip('.')
@@ -37,5 +37,29 @@ def delete_story_media(sender, instance, *args, **kwargs):
 
         if os.path.exists(path):
             shutil.rmtree(path)
+    except Exception as e:
+        logger.warning(f'Story media deletion failed: {e}')
+
+
+@receiver(pre_save, sender=Collection)
+def compress_collection_media_file(sender, instance, **kwargs):
+    try:
+        if not instance.pk:
+            if instance.image:
+                ext = os.path.splitext(instance.image.name)[-1].lower().lstrip('.')
+                
+                if ext in ALLOWED_IMAGE_EXTENSIONS:
+                    instance.image = compress_image(instance.image)
+        else:
+            old_instance = Collection.objects.filter(pk=instance.pk).first()
+
+            if not old_instance:
+                return
+            
+            if instance.image and instance.image != old_instance.image:
+                ext = os.path.splitext(instance.image.name)[-1].lower().lstrip('.')
+
+                if ext in ALLOWED_IMAGE_EXTENSIONS:
+                    instance.image = compress_image(instance.image)
     except Exception as e:
         logger.warning(f'Story media deletion failed: {e}')
